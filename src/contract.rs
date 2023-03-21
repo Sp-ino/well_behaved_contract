902c50a6f876cd63d01990202b3bc4fdcd61a889
sp-ino <spinovale97@gmail.com>
2023-03-21 23:57:06 +0100

    Add state to contract and refactor query()

--- src/contract.rs
+++ src/contract.rs
@@ -1,18 +1,26 @@
 use cosmwasm_std::{
-    entry_point, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult, to_binary,
+    entry_point, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, to_binary,
 };
 use crate::msg::*;
+use crate::state::USERS;
 
 
 
 
 #[entry_point]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: Empty,
+    msg: InstantiateMsg,
 ) -> StdResult<Response> {
+    let users: StdResult<Vec<_>> = msg
+                                    .users
+                                    .into_iter()
+                                    .map(|addr| deps.api.addr_validate(&addr))
+                                    .collect();
+    USERS.save(deps.storage, &users?)?;
+
     Ok(Response::new())
 }
 
@@ -26,23 +34,27 @@ pub fn query(
 ) -> StdResult<Binary> {
 
     match msg {
-        QueryMsg::Greet(grtng) => {
-            let greetresp = GreetResp {
-                greeting: grtng.to_owned(),
-            };
-            to_binary(&greetresp)
-        },
-        QueryMsg::Goodbye(gbye) => {
-            let goodbyeresp = GoodbyeResp {
-                goodbye: gbye.to_owned(),
-            };
-            to_binary(&goodbyeresp)
-        },
+        QueryMsg::Greet(grtng) => to_binary(&greet(grtng)?),
+        QueryMsg::Goodbye(gbye) => to_binary(&say_goodbye(gbye)?),        
     }
 }
 
 
 
-pub fn greet() {
+pub fn greet(gr: String) -> StdResult<GreetResp> {
+    let greetresp = GreetResp {
+        greeting: gr.to_owned(),
+    };
 
+    Ok(greetresp)
+}
+
+
+
+pub fn say_goodbye(gb: String)-> StdResult<GoodbyeResp> {
+    let goodbyeresp = GoodbyeResp {
+        goodbye: gb.to_owned(),
+    };
+
+    Ok(goodbyeresp)
 }
--- src/msg.rs
+++ src/msg.rs
@@ -1,13 +1,26 @@
 use serde::{Serialize, Deserialize};
 
-#[derive(Serialize, Deserialize)]
+// ------------------------Enums that represent messages-----------------------
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
+pub struct InstantiateMsg {
+    pub users: Vec<String>,
+}
+
+
+
+
+#[derive(Serialize, Deserialize, PartialEq, Clone, Debug)]
 pub enum QueryMsg {
+    // Represents a message that triggers
+    // execution of the contract's query()
+    // entry point function
     Greet(String),
     Goodbye(String),
 }
 
 
 
+// ------------------------Structs that represent responses to queries--------
 #[derive(Serialize, Deserialize)]
 pub struct GreetResp {
     pub greeting: String,
